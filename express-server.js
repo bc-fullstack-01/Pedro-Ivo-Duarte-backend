cdc5b2fb071a61fa1adfffc0d665bff73593d976
Pedro Ivo <peustratt@gmail.com>
2022-07-09 20:46:39 -0300

    basic expres.js server

--- express-server.js
+++ express-server.js
@@ -1,12 +1,11 @@
 const app = require('express')();
+PORT = 4000;
 
 app.get('/', (req, res) => {
   console.log(req.headers)
   res.send('Hello from express.js server!')
 })
 
-app.listen((port) => {
-  port = 4000;
-  console.log(`server listening on http://localhost:${port}`);
-  return port;
+app.listen(PORT, () => {
+  console.log(`server listening on http://localhost:${PORT}`);
 })
