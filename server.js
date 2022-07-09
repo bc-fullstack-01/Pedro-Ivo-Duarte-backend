dd4e7d0c6bf9a5a92939d5d93384f00f7cc77a22
Pedro Ivo <peustratt@gmail.com>
2022-07-09 20:11:12 -0300

    initial http server setup with nodemon

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-const http = require('http');
-const { Stream } = require('stream');
+const http = require('http')
 
 const server = http.createServer((req, res) => {
-  if (req.url === '/') {
-    res.write('Hello from Server')
-    res.end()
+  if(req.url === '/') {
+    console.log(req.headers)
+    res.write('hello from http server');
+    res.end();
   }
 });
 
 server.on('connection', (stream) => {
-  console.log('some one connected')
+  console.log('some one connected');
 });
 
 server.listen(4000);
-console.log('Server listen on http://localhost:4000')
+console.log('sever listening on http://localhost:4000');
