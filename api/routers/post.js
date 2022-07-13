98cbd77749699caf08553ea84f6321169fe4fdcc
Pedro Ivo <peustratt@gmail.com>
2022-07-13 19:15:56 -0300

    removed console.log

--- api/routers/post.js
+++ api/routers/post.js
@@ -5,10 +5,7 @@ const router = express.Router();
 router
   .route('/')
   .get((req, res, next) => Promise.resolve()
-    .then(() => {
-      console.log('rodou')
-      Post.find({}).populate('comments')
-      })
+    .then(() => Post.find({}).populate('comments'))
     .then((data) => res.status(200).json(data))
     .catch(err => next(err)))
 
@@ -31,8 +28,8 @@ router
 
   .delete((req, res, next) => Promise.resolve()
     .then(() => Post.deleteOne({ _id: req.params.id }))
-    .then(() => (Comment.deleteMany({post: req.params.id})))
+    .then(() => (Comment.deleteMany({ post: req.params.id })))
     .catch(err => next(err))
   )
 
-  module.exports = router
+module.exports = router
