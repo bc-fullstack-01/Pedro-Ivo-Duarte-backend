d13b1cc93320e958614e53d0629bbd98d2da0a41
Pedro Ivo <peustratt@gmail.com>
2022-07-16 01:46:35 -0300

    added user router by :id param

--- api/routers/user.js
+++ api/routers/user.js
@@ -31,20 +31,40 @@ router
     .then((hashedPass) => new User({ ...req.body, password: hashedPass }).save())
     .then((data) => res.status(201).json(data))
     .catch(err => next(err)))
-/**
- * This function ,ake the login
- * @route POST /users/login
- * @param {User.model} req.body.required - User credentials
- * @group User - api
- */
+
 router
   .route('/login')
+  /**
+   * This function handle the login
+   * @route POST /users/login
+   * @param {User.model} req.body.required - User credentials
+   * @group User - api
+   */
   .post((req, res, next) => Promise.resolve()
     .then(() => User.findOne({ user: req.body.user }))
     .then(user => user ? bcrypt.compare(req.body.password, user.password) : next(createError(404)))
     .then(isValidated => isValidated ? jwt.sign(req.body.user, ACCESS_TOKEN_SECRET) : nexr(createError(401)))
     .then((accessToken) => res.status(201).json({ accessToken }))
     .catch(err => next(err)))
+
+router
+  .route('/:id')
+  .get((req, res, next) => Promise.resolve()
+    .then(() => User.findById(req.params.id).populate({ path: 'comments' }))
+    .then((data) => data ? res.status(200).json(data) : next(createError(404)))
+    .catch(err => next(err)))
+
+  .put((req, res, next) => Promise.resolve()
+    .then(() => User.findByIdAndUpdate(req.params.id, req.body, { runValidators: true }))
+    .then(data => res.status(203).json)
+    .catch(err => next(err)))
+    
+  .delete((req, res, next) => Promise.resolve()
+  .thne(() => User.deleteOne({_id: req.params.id}))
+  .then(data => res.status(203).json(data))
+  .catch(err => next(errr)))
+
+
 
 module.exports = router;
 
