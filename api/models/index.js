c97ca2f179b49b6cdea0020959cef1d1f7698751
Pedro Ivo <peustratt@gmail.com>
2022-07-21 01:20:58 -0300

    db model config && catch errors

--- api/models/index.js
+++ api/models/index.js
@@ -1,19 +1,23 @@
 const mongoose = require('mongoose');
 
-const connect = mongoose.connect('mongodb://localhost:27017/db_development');
+const connect = mongoose.connect(
+  `${process.env.MONGODB || 'mongodb://localhost:27017/db_development'}`,
+  { serverSelectionTimeoutMS: (!process.env.NODE_ENV ? 1000 : 5000) })
+  .catch(err => console.error(err));
+
 exports.Post = require('./post');
 exports.Comment = require('./comment')
 exports.Profile = require('./profile')
 exports.User = require('./user')
 
-mongoose.connection.on('error', (args) => {
-  console.error(`${JSON.stringify(args)}`);
+mongoose.connection.on('error', () => {
+  console.error(`error while connnecting do MongoDB`);
 });
-mongoose.connection.on('connected', (args) => {
-  console.warn(` conneted ${JSON.stringify(args)}`)
+mongoose.connection.on('connected', () => {
+  console.warn(`connected to MongoDB`)
 });
-mongoose.connection.on('disconnected', (args) => {
-  console.error(`${JSON.stringify(args)}`)
+mongoose.connection.on('disconnected', () => {
+  console.error(`disconnected from MongoDB`)
 });
 
 exports.Connection = connect;
