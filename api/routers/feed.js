8bf8a168d7c639aa43f943487a3e559eb4a1dda6
Pedro Ivo <peustratt@gmail.com>
2022-07-31 23:01:20 -0300

    implemented pagination and feed based on createdAt date

--- api/routers/feed.js
+++ api/routers/feed.js
@@ -5,7 +5,8 @@ router
   .route('/')
   /**
    * This functions get posts feed
-   * @route GET /feed
+   * @route GET /feed?page={page}
+   * @param {integer} page.query - current page
    * @group Feed - api
    * @returns {Array.<Posts>} 200 - An array of posts
    * @returns {Error} default - unexpected error
@@ -13,8 +14,15 @@ router
    */
   .get((req, res, next) => Promise.resolve()
     .then(() => Profile.findById(req.user.profile._id))
-    .then((profile) => Post.find({ profile: { $in: profile.following } }).populate('profile'))
+    .then((profile) => Post
+      .find({ profile: { $in: [...profile.following, req.user.profile._id] } })
+      .populate('profile')
+      .limit(10)
+      .skip((req.query.page || 0) * 10)
+      .sort({ createdAt: 'desc' })
+    )
     .then(data => res.status(200).json(data))
-    .catch(err => next(err)))
+    .catch(err => next(err))
+  )
 
-    module.exports = router
+module.exports = router
